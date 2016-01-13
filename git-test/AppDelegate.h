b48f844b75074c48ed634cd8f3325a9b3c978cf5
Lisa-smlies <lisa_smiles@sina.com>
2016-01-13 22:05:54 +0800

    Initial Commit

---
+++ git-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git-test
+//
+//  Created by lisa on 16/1/13.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
